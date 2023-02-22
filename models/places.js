fc2d77819f882259283eec7f3953b3b491ae1127
Laura Brea <laurabrea17@gmail.com>
2023-02-22 16:38:43 -0500

    added seederd folder and seed file

--- models/places.js
+++ models/places.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose')
 const placeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    pic: { type: String, default: 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' },
-    cuisines: { type: String, required: true },
-    city: { type: String, default: 'Anytown' },
-    state: { type: String, default: 'USA' },
+    name: { 
+        type: String, 
+        required: true 
+    },
+    pic: { 
+        type: String, 
+        default: 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    },
+    cuisines: { 
+        type: String, 
+        required: true 
+    },
+    city: { 
+        type: String, 
+        default: 'Anytown' 
+    },
+    state: { 
+        type: String, 
+        default: 'USA' 
+    },
     founded: Number
 })
 
