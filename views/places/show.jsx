234bec94324ace22f9a98276da2bf6a29c3bf847
Laura Brea <laurabrea17@gmail.com>
2023-02-22 16:56:56 -0500

    edited the show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,6 +8,9 @@ function show (data) {
                 <div className="row">
                     <div className="col-sm-6">
                         <img src={data.place.pic} alt={data.place.name}/>
+                        <h3>
+                        Located in {data.place.city}, {data.place.state}
+                        </h3>
                     </div>
                     <div id="placeInfo" className="col-sm-6">
                     <h1>{ data.place.name }</h1>
@@ -15,22 +18,24 @@ function show (data) {
                     <p>No ratings yet</p>
                     <br/>
                     <h2 className="placeInfo">Description</h2>
-                    <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">Delete</button>
+                    </form>
                     </div>
-                </div>
+                    </div>
                 <hr/>
                 <div  className="row">
                     <h2 id="comments">Comments</h2>
                     <p>No comments yet!</p>
                 </div>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                    Edit
-                </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>
+                
             </main>
         </Def>
     )
