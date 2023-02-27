6278bd7a6432858724a29d3a3b31ea7c11e129b3
Laura Brea <laurabrea17@gmail.com>
2023-02-26 20:12:01 -0500

    added comments section to be able to add comments to the places

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -32,29 +32,53 @@ function show (data) {
                         </h3>
                     </div>
                     <div id="placeInfo" className="col-sm-6">
-                    <h1>{ data.place.name }</h1>
-                    <h2 className="placeInfo">Rating</h2>
-                    <p>No ratings yet</p>
-                    <br/>
-                    <h2 className="placeInfo">Description</h2>
-                    <h3>
+                        <h1>{ data.place.name }</h1>
+                        <h2 className="placeInfo">Rating</h2>
+                        <p>No ratings yet</p>
+                        <br/>
+                        <h2 className="placeInfo">Description</h2>
+                        <h3>
                         {data.place.showEstablished()}
-                    </h3>
-                    <h4>
+                        </h3>
+                        <h4>
                         Serving {data.place.cuisines}
-                    </h4>
-                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
-                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                        <button type="submit" className="btn btn-danger">Delete</button>
-                    </form>
+                        </h4>
+                            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">Delete</button>
+                        </form>
                     </div>
-                    </div>
+                </div>
                 <hr/>
                 <div  className="row">
                     <h2 id="comments">Comments</h2>
                     <p>{comments}</p>
                 </div>
-                
+                <hr/>
+                <h2>Got Your Own Rant or Rave?</h2>
+                <form action={`/places/${data.place.id}/comment`} method="POST">
+                    <div className="row">
+                        <div className="form-group col-sm-12">
+                            <label htmlFor="content">Content</label>
+                            <textarea id="content" name="content" className="form-control"></textarea>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="author">Author</label>
+                            <input id="author" name="author" className="form-control"/>
+                        </div>
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="stars">Star Rating</label>
+                            <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control"/>
+                        </div>
+                        <div className="form-group col-sm-2">
+                            <label htmlFor="rant">Rant?</label>
+                            <input type="checkbox" id="rant" name="rant" className="form-control"/>
+                        </div>
+                    </div>
+                    <input type="submit" className="btn btn-primary" value="Add Comment"/>
+                </form>
             </main>
         </Def>
     )
